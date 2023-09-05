b9741bd46b64a0ff46643c884dec188e8cea0f6f
Vyappi12 <100416853+Vyappi12@users.noreply.github.com>
2023-09-05 11:24:05 +0300

    Create 6.py

---
+++ 6.py
@@ -0,0 +1,24 @@
+def lsl(x, left, middle, right):
+  if x[3] == 2018:
+    return left
+  if x[3] == 1974
+    return middle
+  return right
+
+def ejs(x, left, middle, right):
+  if x[2] == 2004:
+    return left
+  if x[2] == 1988
+    return middle
+  return right
+
+def main():
+  if x[4] == 1985:
+    if x[1] == 'LSL':
+      return lsl(x, 0, ejs(x, 1, 2, 3), 4)
+    if x[1] == 'EJS':
+      return lsl(x, 5, ejs(x, 6, 7, 8), 9)
+    else:
+      return 10
+  return 11
+      
